cd1aa2f42d7763882bbdd09f22b4899dfa0d03a3
seanrw93 <seanrw@gmail.com>
2026-07-02 18:29:37 +0200

    fix: add @storybook/addon-controls to addons — required explicitly in Storybook v8

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -7,6 +7,7 @@ const config: StorybookConfig = {
   ],
   addons: [
     '@chromatic-com/storybook',
+    '@storybook/addon-controls',
     '@storybook/addon-a11y',
     '@storybook/addon-docs',
   ],
